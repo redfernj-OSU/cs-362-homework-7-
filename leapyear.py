94a7d2304656249e2196c4ab2177a30442493744
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-05-28 01:43:23 -0700

    implement failed leap years

--- leapyear.py
+++ leapyear.py
@@ -2,8 +2,11 @@ def leapyear(year):
     if (year % 4) == 0:
         return True
 
-    if (year % 100) == 0:
+    elif (year % 100) == 0:
         return True
 
-    if (year % 400) == 0:
+    elif (year % 400) == 0:
         return True
+
+    else:
+        return False
